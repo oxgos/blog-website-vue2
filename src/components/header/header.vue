21b264f951a2d6a2685bcf98f80c87fd7bb48046
gjw <122486975@qq.com>
2017-11-01 11:07:05 +0800

    complete website model

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="header">
-      <header class="head">我是头部</header>
+      <h1>如影随形</h1>
+      <h2>影子是一个会撒谎的精灵，它在虚空中流浪和等待被发现之间;在存在与不存在之间....</h2>
       <nav class="nav">
           <ul>
               <li class="active"><router-link to="/">首页</router-link></li>
